63e189a830083ffa5ae58b84fdab75dc2a2368de
Tuğba Kocabaş <14tuba@gmail.com>
2024-04-09 18:48:56 +0300

    Güncelleme 9
    Main menu ve ses

--- Assets/_GAME/Scripts/UIController.cs
+++ Assets/_GAME/Scripts/UIController.cs
@@ -6,14 +6,40 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     public Slider _musicSlider, _sfxSlider;
+    public Image _musicImage, _sfxImage;
+    public Sprite _musicOnSprite, _musicOffSprite, _sfxOnSprite, _sfxOffSprite;
 
     public void ToggleMusic()
     {
         AudioManager.Instance.ToggleMusic();
+        UpdateMusicImage();
     }
     public void ToggleSFX()
     {
         AudioManager.Instance.ToggleSFX();
+        UpdateSFXImage();
+    }
+    private void UpdateMusicImage()
+    {
+        if (AudioManager.Instance.IsMusicMuted())
+        {
+            _musicImage.sprite = _musicOffSprite;
+        }
+        else
+        {
+            _musicImage.sprite = _musicOnSprite;
+        }
+    }
+    private void UpdateSFXImage()
+    {
+        if (AudioManager.Instance.IsSFXMuted())
+        {
+            _sfxImage.sprite = _sfxOffSprite;
+        }
+        else
+        {
+            _sfxImage.sprite = _sfxOnSprite;
+        }
     }
     public void MusicVolume()
     {
