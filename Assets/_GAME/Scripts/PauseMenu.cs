17273e25bb50a84e232055f19a6c766327d66159
Soner Keskin <soner.keskiin@gmail.com>
2024-04-12 22:38:42 +0300

    Güncelleme
    Input settings update

--- Assets/_GAME/Scripts/PauseMenu.cs
+++ Assets/_GAME/Scripts/PauseMenu.cs
@@ -4,24 +4,48 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        isPaused = true;
     }
+
     public void Home()
     {
         SceneManager.LoadScene("Main Menu");
         Time.timeScale = 1;
+        isPaused = false;
     }
+
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
+        isPaused = false;
     }
+
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
+        isPaused = false;
     }
 }
