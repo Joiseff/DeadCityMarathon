b5b51993496a78e7538491c4c9555a7259af2368
Soner Keskin <soner.keskiin@gmail.com>
2024-04-13 19:53:17 +0300

    Revert "Güncelleme"
    This reverts commit a588ae685431a58948867904667bb1aeb900d3a5.

--- Assets/_GAME/Scripts/DieMenu.cs
+++ Assets/_GAME/Scripts/DieMenu.cs
@@ -1,25 +1,19 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DieMenu : MonoBehaviour
 {
     [SerializeField] GameObject dieMenu;
-    [SerializeField] GameObject pauseButton;
+
+
 
     
 
-    private void Start()
-    {
-        
-    }
     public void DMenu()
     {
         dieMenu.SetActive(true);
         Time.timeScale = 0;
-        pauseButton.SetActive(false);
- 
-
+        
     }
 
 
@@ -35,6 +29,6 @@ public class DieMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
-
+        
     }
 }
